75ba339bf1b189b74a5f794ac05856f33c1f84e6
ERWIN <ERWIN@LAPTOP-68DKNHF3>
2022-04-06 17:37:35 -0500

    Se ha agregado un saludo a Kiara

--- src/pucp/edu/pe/Main/Principal.java
+++ src/pucp/edu/pe/Main/Principal.java
@@ -13,6 +13,7 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo...!");
         System.out.println("Hola Makito...");
+        System.out.println("Hola Kiara");
     }
     
 }
