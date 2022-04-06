fe4541e7a813ebab7fb4ae0fff1116f4eb8cdbfd
ERWIN <ERWIN@LAPTOP-68DKNHF3>
2022-04-06 17:31:55 -0500

    Se ha agregado un saludo a Makito

--- src/pucp/edu/pe/Main/Principal.java
+++ src/pucp/edu/pe/Main/Principal.java
@@ -12,7 +12,7 @@ package pucp.edu.pe.Main;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo...!");
-        System.err.println("Hola Makito...");
+        System.out.println("Hola Makito...");
     }
     
 }
